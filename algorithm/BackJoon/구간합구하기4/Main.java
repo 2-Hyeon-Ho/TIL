7a2721c5923f2f159e59ee2d829c6c378a97f0e4
2HyeonHo <dpqlvkvk@naver.com>
2023-01-30 19:19:17 +0900

    구간합 구하기4

--- algorithm/BackJoon/구간합구하기4/Main.java
+++ algorithm/BackJoon/구간합구하기4/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        Scanner sc =  new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] array = new int[a];
+        int[] number = new int[a+1];
+        for(int i = 0; i < a; i++) {
+            array[i]=sc.nextInt();
+        }
+        number[0] = 0;
+        for(int i = 1; i < a+1; i++) {
+            number[i] = number[i-1] + array[i-1];
+        }
+        for(int i = 0; i < b; i++) {
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            System.out.println(number[d]-number[c-1]);
+        }
+    }
 }
